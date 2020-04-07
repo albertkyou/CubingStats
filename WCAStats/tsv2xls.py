04a441cc58d765d55f6ff24344bcae9049bedd14
albertkyou <albert.you@berkeley.edu>
2020-04-07 10:49:48 -0700

    Converted tsv to csv

--- WCAStats/tsv2xls.py
+++ WCAStats/tsv2xls.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats 
 import pandas as pd 
 
-df = pd.read_csv('WCA_export_Results.tsv','\t')
+df = pd.read_csv('D:\GitHub\CubingStats\WCAStats\WCA_export_Results.tsv','\t')
 
-print(df.head())
+threespeed = df.loc[df.eventId=='333']
+
+threespeed.to_excel('threespeed_results.xlsx')
